a1f50348476059418e6481166fe2a99a72cb3cee
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2026-05-19 10:16:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,11 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8m_custom.pt")
 
-model.predict(source="1.mp4", show=True, conf=0.5, line_thickness=1)
+model.predict(
+    source=0,
+    show=True,
+    conf=0.5,
+    imgsz=640,
+    device=0,  
+    half=True
+)
